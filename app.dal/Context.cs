fcef2f40d7304a5809939228da837f7defe924a2
ask098 <cmlocastro20@gmail.com>
2018-12-16 14:13:02 -0500

    update initial project

---
+++ app.dal/Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using app.entities;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace app.dal
+{
+    public class Context : DbContext
+    {
+        public Context(DBcontextOptions options) : base(options)
+        {
+        }
+        public DbSet<Academy> Academies { get; set; }
+
+        protected override void OnModeCreating(ModelBuilder builder)
+        {
+            builder.Entity<Academy>().hasData(
+                new Academy { Id = 1, Email = "mail@mail.com", Name= "test"}
+            );
+        }
+
+
+    }
+}
