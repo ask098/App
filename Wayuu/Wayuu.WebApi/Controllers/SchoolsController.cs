273ec40a3d19fd78f97510b004ffd396dd59ea51
Hugo Rodriguez <hrodrime@gmail.com>
2019-02-04 23:16:15 -0500

    Creación de WEB API

---
+++ Wayuu/Wayuu.WebApi/Controllers/SchoolsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Wayuu.Entities;
+using Wayuu.Services;
+
+namespace Wayuu.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolsController : ControllerBase
+    {
+        ISchoolOperations Helper;
+
+        public SchoolsController(ISchoolOperations schoolOperations)
+        {
+            this.Helper = schoolOperations;
+        }
+
+        [HttpGet]
+        public IEnumerable<School> Get()
+        {
+            return Helper.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "CreatedSchool")]
+        public IActionResult GetById(int id)
+        {
+            var SchoolFound = Helper.RetrieveSchoolById(id);
+            if (SchoolFound == null)
+            {
+                return NotFound();
+            }
+            return Ok(SchoolFound);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] School newSchool)
+        {
+            if (ModelState.IsValid)
+            {
+                var School = Helper.Create(newSchool);
+                return new CreatedAtRouteResult("CreatedSchool", new { id = School.Id }, School);
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
