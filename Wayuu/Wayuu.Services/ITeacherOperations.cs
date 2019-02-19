700a1c57b207481d6c2a36159ac5570b2ebbb2e1
Hugo Rodriguez <hrodrime@gmail.com>
2019-02-19 00:21:09 -0500

    1er avance teacher

---
+++ Wayuu/Wayuu.Services/ITeacherOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wayuu.Entities;
+
+namespace Wayuu.Services
+{
+    public interface ITeacherOperations
+    {
+        Teacher Create(Teacher teacher);
+        Teacher RetrieveById(int teacherId);
+        bool Update(Teacher teacher);
+        bool Delete(int teacherId);
+        List<Teacher> GetAll();
+
+        bool DeleteWithLog(int teacherId);
+    }
+}
